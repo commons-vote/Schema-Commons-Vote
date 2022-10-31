efd58533c56c7ad06ba8bd8ae46ed883d2e299f1
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-10-31 10:15:10 +0100

    Add validation_bad table to db

---
+++ Vote/0_1_0/Result/ValidationBad.pm
@@ -0,0 +1,39 @@
+package Schema::Commons::Vote::0_1_0::Result::ValidationBad;
+
+use base qw(DBIx::Class::Core);
+use strict;
+use warnings;
+
+our $VERSION = 0.01;
+
+__PACKAGE__->load_components('InflateColumn::DateTime');
+__PACKAGE__->table('validation_bad');
+__PACKAGE__->add_columns(
+	'competition_id' => {
+		'data_type' => 'integer',
+	},
+	'image_id' => {
+		'data_type' => 'integer',
+	},
+	'validation_type_id' => {
+		'data_type' => 'integer',
+	},
+	'created_by_id' => {
+		'data_type' => 'integer',
+	},
+	'created_at' => {
+		'data_type' => 'datetime',
+		'default_value' => 'CURRENT_TIMESTAMP',
+	},
+);
+__PACKAGE__->add_unique_constraint(
+	'validation_bad_unique_key' => ['competition_id', 'image_id', 'validation_type_id'],
+);
+__PACKAGE__->belongs_to('competition' => 'Schema::Commons::Vote::0_1_0::Result::Competition', 'competition_id');
+__PACKAGE__->belongs_to('created_by' => 'Schema::Commons::Vote::0_1_0::Result::Person', 'created_by_id');
+__PACKAGE__->belongs_to('image' => 'Schema::Commons::Vote::0_1_0::Result::Image', 'image_id');
+__PACKAGE__->belongs_to('validation_type' => 'Schema::Commons::Vote::0_1_0::Result::ValidationType', 'validation_type_id');
+
+1;
+
+__END__
